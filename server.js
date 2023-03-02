1a3cee76001c60e47ece1801bf6cc19a7104d779
sanderpalk <sanderpalk0@gmail.com>
2023-03-02 14:55:59 +0200

    Removed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import fs from "fs";
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
 const app = express();
 
 app.use(cors({
